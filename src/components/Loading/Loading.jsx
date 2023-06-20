27f73f06f53af2292dc10cf0afa367e500fc496e
gabriel marucci <gabrielmarucci777@hotmail.com>
2023-06-20 13:32:24 -0300

    first commit

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Loading.css';
+import { BiLoaderCircle } from 'react-icons/bi';
+
+
+function Loading() {
+  return <BiLoaderCircle className="loading"/>;
+    
+
+}
+
+export default Loading;
